57fbbee8fa96e1f77ad477460576a525bfb0970b
salimsulaiman <salimsulaiman75@gmail.com>
2023-05-03 13:08:09 +0700

    finish login register

---
+++ src/js/login.js
@@ -0,0 +1,34 @@
+const BASEURL = "https://6438a4841b9a7dd5c9554920.mockapi.io";
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const loginForm = document.getElementById("loginForm");
+const notif = document.getElementById("notif");
+const btnLogin = document.getElementById("login");
+
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const user = {
+    email: emailInput.value,
+    password: password.value,
+  };
+
+  let res = await fetch(`${BASEURL}/users`);
+  let item = await res.json();
+  let tempData = [];
+  for (let i = 0; i < item.length; i++) {
+    if (item[i].email == user.email) {
+      tempData.push(item[i]);
+    }
+  }
+
+  if (tempData.length < 1) {
+    notif.innerHTML = `<div class="alert alert-danger text-center" role="alert">
+    Email atau Password anda salah.`;
+  } else if (tempData[0].email == user.email && tempData[0].password == user.password) {
+    localStorage.setItem(`id`, `${tempData[0].uid}`);
+    location.href = "../../index.html";
+  } else {
+    notif.innerHTML = `<div class="alert alert-danger text-center" role="alert">
+    Email atau Password anda salah.`;
+  }
+});
